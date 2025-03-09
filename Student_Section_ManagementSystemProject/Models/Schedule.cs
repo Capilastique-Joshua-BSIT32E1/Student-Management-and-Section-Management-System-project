41911d585e9ebd9b0b24ec031d4cc138b530ec70
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 17:21:34 +0800

    Update the Schedule Models

--- Student_Section_ManagementSystemProject/Models/Schedule.cs
+++ Student_Section_ManagementSystemProject/Models/Schedule.cs
@@ -1,4 +1,7 @@
-﻿namespace Student_Section_ManagementSystemProject.Models
+﻿using Student_Section_ManagementSystemProject.Models;
+using System.Collections.Generic;
+
+namespace Student_Section_ManagementSystemProject.Models
 {
     public class Schedule
     {
@@ -7,5 +10,6 @@
         public Subject Subject { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
+        public List<Student> EnrolledStudents { get; set; } = new List<Student>();
     }
 }
