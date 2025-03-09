de3a10ef767b48a7a5158585141ab9988cc92fdb
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 15:28:42 +0800

    Add ApplicationDbContext.cs

---
+++ Student_Section_ManagementSystemProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Section_ManagementSystemProject.Data
+{
+    public class ApplicationDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
