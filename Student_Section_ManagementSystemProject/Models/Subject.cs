922c9026ef70bc1d13969c51f236d0f154e7d8fe
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 18:59:05 +0800

    Updated Subject Model

--- Student_Section_ManagementSystemProject/Models/Subject.cs
+++ Student_Section_ManagementSystemProject/Models/Subject.cs
@@ -4,9 +4,11 @@ public class Subject
 {
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "Name is required.")]
+    [Required(ErrorMessage = "Subject Name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
     public string Name { get; set; }
 
     [Required(ErrorMessage = "Description is required.")]
+    [StringLength(250, ErrorMessage = "Description cannot exceed 250 characters.")]
     public string Description { get; set; }
 }
