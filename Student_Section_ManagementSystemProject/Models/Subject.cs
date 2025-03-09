6a1de505ff20f1c451d5f3336cad191a43320b7b
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 17:40:48 +0800

    update model subject

--- Student_Section_ManagementSystemProject/Models/Subject.cs
+++ Student_Section_ManagementSystemProject/Models/Subject.cs
@@ -1,9 +1,12 @@
-﻿namespace Student_Section_ManagementSystemProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Subject
 {
-    public class Subject
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Description is required.")]
+    public string Description { get; set; }
 }
