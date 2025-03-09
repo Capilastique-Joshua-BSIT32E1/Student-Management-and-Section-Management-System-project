26737a5e1e9552a9ac0d997e7bc13f2738c0748a
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 19:51:14 +0800

    Update DB

--- Student_Section_ManagementSystemProject/Data/ApplicationDbContext.cs
+++ Student_Section_ManagementSystemProject/Data/ApplicationDbContext.cs
@@ -8,7 +8,24 @@ namespace Student_Section_ManagementSystemProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Student> Students { get; set; }
-        public DbSet<Subject> Subjects { get; set; }
         public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Enrollment>()
+                .HasKey(e => new { e.StudentId, e.ScheduleId }); // Composite Key
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.StudentId);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Schedule)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.ScheduleId);
+        }
     }
 }
