fef72e91bff480d82bbf0bb4906dc6460e2b1b3f
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 17:25:21 +0800

    Add EnrollmentController

---
+++ Student_Section_ManagementSystemProject/Controllers/EnrollmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Section_ManagementSystemProject.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
