038de63ab2a4d4c05e544dca55644cfbf6e518e8
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 15:44:36 +0800

    Add SubjectsControllers

---
+++ Student_Section_ManagementSystemProject/Controllers/SubjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Section_ManagementSystemProject.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
