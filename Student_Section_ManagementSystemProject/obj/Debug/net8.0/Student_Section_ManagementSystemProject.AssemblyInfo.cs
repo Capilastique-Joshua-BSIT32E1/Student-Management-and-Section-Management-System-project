1830de87317b755fad6e7e0fa632c8d547aaf798
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 20:19:14 +0800

    Updating

--- Student_Section_ManagementSystemProject/obj/Debug/net8.0/Student_Section_ManagementSystemProject.AssemblyInfo.cs
+++ Student_Section_ManagementSystemProject/obj/Debug/net8.0/Student_Section_ManagementSystemProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student_Section_ManagementSystemProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bbd87e50474143b0316eb9571618468d6ac7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student_Section_ManagementSystemProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student_Section_ManagementSystemProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
