40659ce6755a2521ab7b66fc5caa17b842999e4e
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 15:43:57 +0800

    Add code to StudentsController.cs

--- Student_Section_ManagementSystemProject/Controllers/StudentsController.cs
+++ Student_Section_ManagementSystemProject/Controllers/StudentsController.cs
@@ -1,12 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Student_Section_ManagementSystemProject.Data;
+using Student_Section_ManagementSystemProject.Models;
+using System.Linq;
 
 namespace Student_Section_ManagementSystemProject.Controllers
 {
     public class StudentsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index() => View(_context.Students.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public IActionResult Edit(int id) => View(_context.Students.Find(id));
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            _context.Students.Update(student);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
