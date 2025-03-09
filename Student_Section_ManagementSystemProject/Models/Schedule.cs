bdab39fc07c5b193776e821812837e7fc2856413
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 15:40:59 +0800

    Add code to schedule.cs

--- Student_Section_ManagementSystemProject/Models/Schedule.cs
+++ Student_Section_ManagementSystemProject/Models/Schedule.cs
@@ -2,5 +2,10 @@
 {
     public class Schedule
     {
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
     }
 }
