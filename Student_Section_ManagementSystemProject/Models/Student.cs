2dcad8bdc32d768ea9db5e274b47066b7b118059
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 16:44:00 +0800

    Add validation

--- Student_Section_ManagementSystemProject/Models/Student.cs
+++ Student_Section_ManagementSystemProject/Models/Student.cs
@@ -1,9 +1,16 @@
-﻿namespace Student_Section_ManagementSystemProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Section_ManagementSystemProject.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
     }
 }
