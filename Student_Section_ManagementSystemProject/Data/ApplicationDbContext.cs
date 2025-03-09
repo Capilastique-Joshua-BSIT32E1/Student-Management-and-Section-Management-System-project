1a8762f9f982256794ed62f14ed5c4df59e9d6de
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 15:36:24 +0800

    Add code to ApplicationDbContext

--- Student_Section_ManagementSystemProject/Data/ApplicationDbContext.cs
+++ Student_Section_ManagementSystemProject/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Student_Section_ManagementSystemProject.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Section_ManagementSystemProject.Models;
+
+namespace Student_Section_ManagementSystemProject.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
     }
 }
