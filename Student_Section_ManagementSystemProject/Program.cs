50177ce01793921a46507ae23eed00510f8e4c61
CapilastiqueJoshua-BSIT32E1 <161306871+Capilastique-Joshua-BSIT32E1@users.noreply.github.com>
2025-03-09 15:50:12 +0800

    Update Program.cs

--- Student_Section_ManagementSystemProject/Program.cs
+++ Student_Section_ManagementSystemProject/Program.cs
@@ -7,24 +7,26 @@ using Student_Section_ManagementSystemProject.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("StudentDB"));
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
+        app.MapControllerRoute(
+            name: "default",
             pattern: "{controller=Students}/{action=Index}/{id?}");
 
 app.Run();
